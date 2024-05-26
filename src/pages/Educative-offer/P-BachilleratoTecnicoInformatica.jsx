2818bf8d3296d29377e76b49af364a663c1caf80
joelgbs <arielperinguez66@gmail.com>
2024-05-25 22:54:47 -0500

    contenido en las pages de oferta educativa y se cre un componete de min-portada v1.9

--- src/pages/Educative-offer/P-BachilleratoTecnicoInformatica.jsx
+++ src/pages/Educative-offer/P-BachilleratoTecnicoInformatica.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
+import Cnav from '../../components/C-nav.jsx';
+import {NavMenuMobile, NavMenuMobileButton} from '../../components/C-nav-menu-mobile.jsx';
+import Minportada from '../../components/C-min-portada.jsx';
+import fotoportada from '../../assets/colegio/foto-patio-1.jpg';
 
 function Bti(){
+  const [menuVisible, setMenuVisible] = useState(false);
+  const showMenu = () => {
+    setMenuVisible(true);
+    document.body.style.overflow = 'hidden'; // Deshabilita los scrolls en el body
+  };
+  const hideMenu = () => {
+    setMenuVisible(false);
+    document.body.style.overflow = ''; // Habilita los scrolls en el body
+  };
     return(
       <div>
-        <h1>hola</h1>
+        {menuVisible && <NavMenuMobile BotonExitmenufloat={hideMenu} />}
+        <div className="section-page">
+        <Cnav showMenu={showMenu} />
+        <Minportada
+          imagen={fotoportada}
+          titulo='Bachilletaro Tecnico Informatica'
+        />
+        </div>
       </div>
     );
 }
--- src/pages/Educative-offer/P-EducacionInicial.jsx
+++ src/pages/Educative-offer/P-EducacionInicial.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
+import Cnav from '../../components/C-nav.jsx';
+import {NavMenuMobile, NavMenuMobileButton} from '../../components/C-nav-menu-mobile.jsx';
+import Minportada from '../../components/C-min-portada.jsx';
+import fotoportada from '../../assets/colegio/foto-patio-1.jpg';
+
 
 function EducacionInicial(){
+  const [menuVisible, setMenuVisible] = useState(false);
+  const showMenu = () => {
+    setMenuVisible(true);
+    document.body.style.overflow = 'hidden'; // Deshabilita los scrolls en el body
+  };
+  const hideMenu = () => {
+    setMenuVisible(false);
+    document.body.style.overflow = ''; // Habilita los scrolls en el body
+  };
     return(
-      <div >
-        <h1>hola a</h1>
+      <div className="Einicial">
+        {menuVisible && <NavMenuMobile BotonExitmenufloat={hideMenu} />}
+        <div className="section-page">
+        <Cnav showMenu={showMenu} />
+        <Minportada
+          imagen={fotoportada}
+          titulo='Educacion Inicial'
+        />
+        </div>
       </div>
     );
 }
